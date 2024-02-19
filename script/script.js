95b575ae5607c9ae6a665feda067d5f30cbb8875
Deep Modi <47850094+DeepModiDev@users.noreply.github.com>
2024-02-19 22:58:32 +0530

    Fixed some Home page UI related issues and improved design. Added script for side navigation and header.

--- script/script.js
+++ script/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // For Navigation Header
+   /* // For Navigation Header
     // Get all menu items
     const menuItems = document.querySelectorAll('.header-menu-item');
 
@@ -35,9 +35,9 @@ document.addEventListener('DOMContentLoaded', function () {
             // Add the "active" class to the clicked menu item
             item.querySelector('a').classList.add('active');
         });
-    });
+    });*/
 
-    //For side navigation view
+  /*  //For side navigation view
     // Get all menu items
     const menuItemsNav = document.querySelectorAll('#side-menu-id li');
 
@@ -53,6 +53,53 @@ document.addEventListener('DOMContentLoaded', function () {
             // Add the "active" class to the clicked menu item
             item.querySelector('a').classList.add('active');
         });
-    });
+    });*/
 });
+
+window.addEventListener('scroll', function() {
+    var headerHeight = document.getElementById('header').offsetHeight;
+    var homeSection = document.getElementById('home');
+    var aboutSection = document.getElementById('about');
+    var skillsSection = document.getElementById('skills');
+    var projectsSection = document.getElementById('projects');
+    var contactSection = document.getElementById('contact');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition < homeSection.offsetTop + homeSection.offsetHeight - headerHeight) {
+        setActiveLink('item0');
+        setActiveLinkSideMenu('li-home');
+    } else if (scrollPosition < aboutSection.offsetTop + aboutSection.offsetHeight - headerHeight) {
+        setActiveLink('item1');
+        setActiveLinkSideMenu('li-about');
+    } else if (scrollPosition < skillsSection.offsetTop + skillsSection.offsetHeight - headerHeight) {
+        setActiveLink('item2');
+        setActiveLinkSideMenu('li-skills');
+    } else if (scrollPosition < projectsSection.offsetTop + projectsSection.offsetHeight - headerHeight) {
+        setActiveLink('item3');
+        setActiveLinkSideMenu('li-project');
+    } else {
+        setActiveLink('item4');
+        setActiveLinkSideMenu('li-contact');
+    }
+});
+
+function setActiveLink(itemId) {
+    var navbarLinks = document.querySelectorAll('.header-menu-item a');
+    navbarLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
+    document.getElementById(itemId).querySelector('a').classList.add('active');
+}
+
+function setActiveLinkSideMenu(itemId){
+    const sideMenu = document.getElementById('side-menu-id');
+    const links = sideMenu.querySelectorAll('a');
+
+    links.forEach(link => {
+        link.classList.remove('active');
+    });
+
+    document.getElementById(itemId).querySelector('a').classList.add('active');
+}
+
 
